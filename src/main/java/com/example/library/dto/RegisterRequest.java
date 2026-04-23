84a8ab9fcc31e376e8f929eefbccccffa48ec1dc
MsdAmine <moussaid.amine19@gmail.com>
2026-04-24 00:30:03 +0100

    feat: implement JWT-based authentication and security configuration using Spring Security

---
+++ src/main/java/com/example/library/dto/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.example.library.dto;
+
+import com.example.library.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private Role role;
+}
---
+++ src/main/java/com/example/library/service/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.example.library.service;
+
+import com.example.library.dto.AuthenticationRequest;
+import com.example.library.dto.AuthenticationResponse;
+import com.example.library.dto.RegisterRequest;
+import com.example.library.model.Role;
+import com.example.library.model.User;
+import com.example.library.repository.UserRepository;
+import com.example.library.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole() == null ? Role.USER : request.getRole())
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
